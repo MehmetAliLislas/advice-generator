f347a31b2c0bc945ca5993b99121d942c757a1cc
MehmetAliLislas <mehmetalilislas@gmail.com>
2024-07-28 14:44:13 +0300

    update index.js

--- index.js
+++ index.js
@@ -2,20 +2,8 @@ const url = "https://api.adviceslip.com/advice";
 
 const newAdvice = () => {
   setTimeout(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => getAdvice(json))
-      .catch((err) => console.log(err));
-
-    const getAdvice = (json) => {
-      let adviceID = json.slip.id;
-      let adviceData = json.slip.advice;
-      let adviceNumber = document.querySelector(".advice-number");
-      adviceNumber.innerHTML = `ADVICE #${adviceID}`;
-      let advice = document.querySelector(".advice");
-      advice.innerHTML = `"${adviceData}"`;
-    };
-  }, 0);
+    getAdvice();
+  }, 1000);
 };
 
 const getAdvice = () => {
